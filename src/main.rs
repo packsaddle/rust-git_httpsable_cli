e9f2dfde2f2d231c63178d5c782f33ba232f4d96
Sanemat <o.gata.ken@gmail.com>
2017-10-15 00:04:19 +0900

    chore(main): read env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
 fn main() {
+    let username = env::var("GIT_HTTPSABLE_USERNAME").unwrap();
+    let password = env::var("GIT_HTTPSABLE_PASSWORD").unwrap();
     println!("Hello, world!");
 }
