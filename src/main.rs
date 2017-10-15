6c87635f527aa9fab2b48936ecd104fa7e1ebf30
Sanemat <o.gata.ken@gmail.com>
2017-10-15 14:13:40 +0900

    chore(main): implement image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
+    let username = env::var("GIT_HTTPSABLE_USERNAME").unwrap();
+    let password = env::var("GIT_HTTPSABLE_PASSWORD").unwrap();
+    let repo_url = "https://github.com/packsaddle/example-circle_ci-pull_request.git";
+    let target_dir = "./target_dir";
+    //    git httpsable clone --depth=1
     println!("Hello, world!");
 }
