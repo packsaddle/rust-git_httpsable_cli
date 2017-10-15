b6b500cc4f189954c7a35ff8441c0d9c4aa1c150
Sanemat <o.gata.ken@gmail.com>
2017-10-15 19:03:06 +0900

    chore(main): use command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::env;
+use std::process::{Command, Stdio};
 
 fn main() {
-    let username = env::var("GIT_HTTPSABLE_USERNAME").unwrap();
-    let password = env::var("GIT_HTTPSABLE_PASSWORD").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let username = match env::var("GIT_HTTPSABLE_USERNAME") {
+        Ok(result) => result,
+        Err(_) => panic!("GIT_HTTPSABLE_USERNAME is required.")
+    };
+    let password = match env::var("GIT_HTTPSABLE_PASSWORD") {
+        Ok(result) => result,
+        Err(_) => panic!("GIT_HTTPSABLE_PASSWORD is required.")
+    };
     let repo_url = "https://github.com/packsaddle/example-circle_ci-pull_request.git";
-    let target_dir = "./target_dir";
+    let target_dir = "./for_target_dir";
     //    git httpsable clone --depth=1
-    println!("Hello, world!");
+    let mut child = Command::new("git")
+        .args(&["clone", "--depth", "1", repo_url, target_dir])
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("git command failed to start");
+    let ecode = child.wait().expect("failed to wait on child");
+
+    assert!(ecode.success());
 }
